70d9ba278ece52bd1985e5c747537df535d5a4c5
yxx <yxx_beijing@163.com>
2021-03-30 16:39:04 +0800

    test

---
+++ src/main/java/com/imooc/interceptor/MyInterceptor.java
@@ -0,0 +1,7 @@
+package com.imooc.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class MyInterceptor implements HandlerInterceptor {
+
+}
---
+++ src/main/java/com/imooc/interceptor/Test.java
@@ -0,0 +1,4 @@
+package com.imooc.interceptor;
+
+public class Test {
+}
