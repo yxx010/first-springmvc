275c2c00aa270fb691646c2a7d1f3a0efa4a41ee
yxx <yxx_beijing@163.com>
2021-03-31 16:51:01 +0800

    spring mvc过滤器

---
+++ src/main/java/com/imooc/interceptor/AccessHistoryInterceptor.java
@@ -0,0 +1,23 @@
+package com.imooc.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AccessHistoryInterceptor implements HandlerInterceptor {
+    private Logger logger= LoggerFactory.getLogger(AccessHistoryInterceptor.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        StringBuilder log=new StringBuilder();
+        log.append(request.getRemoteAddr());
+        log.append("|");
+        log.append(request.getRequestURL());
+        log.append("|");
+        log.append(request.getHeader("user-agent"));
+        log.append("|");
+        logger.info(log.toString());
+        return true;
+    }
+}
--- src/main/java/com/imooc/interceptor/MyInterceptor1.java
+++ src/main/java/com/imooc/interceptor/MyInterceptor1.java
@@ -8,21 +8,21 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Spring mvc拦截器 使用需要在配置文件中配置
+ * 多拦截器正常访问时方法执行顺序pre1-->pre2--target(controller)-->post2-->post1-->after2-->after1
  */
-public class MyInterceptor implements HandlerInterceptor {
+public class MyInterceptor1 implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println(request.getRequestURL()+"-准备执行");
-        //true会继续执行后续两个步骤，false就只执行到这
+        System.out.println(request.getRequestURL()+"-准备执行-1");
         return true;
     }
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println(request.getRequestURL()+"-目标处理成功");
+        System.out.println(request.getRequestURL()+"-目标处理成功-1");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println(request.getRequestURL()+"-响应内容已产生");
+        System.out.println(request.getRequestURL()+"-响应内容已产生-1");
     }
 }
